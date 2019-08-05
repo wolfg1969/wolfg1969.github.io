8fe5921d18d930ce340a1fd12a57baa7f72b68f4
wolfg1969 <wolfg1969@gmail.com>
2019-08-05 12:07:49 +0800

    site archive

---
+++ blog/wp-login.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="zh-CN">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>登录 &lsaquo; 郭勇的技术博客 &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='/blog/wp-admin/load-scripts.php?c=1&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.9.10'></script>
+<script type='text/javascript' src='/blog/wp-content/plugins/easy-social-icons/js/cnss.js?ver=1.0'></script>
+<link rel='stylesheet' href='/blog/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.10' type='text/css' media='all' />
+<link rel='stylesheet' id='cnss_font_awesome_css-css'  href='/blog/wp-content/plugins/easy-social-icons/css/font-awesome/css/font-awesome.min.css?ver=4.7' type='text/css' media='all' />
+<link rel='stylesheet' id='cnss_css-css'  href='/blog/wp-content/plugins/easy-social-icons/css/cnss.css?ver=1.0' type='text/css' media='all' />
+	<meta name='robots' content='noindex,noarchive' />
+	<meta name='referrer' content='strict-origin-when-cross-origin' />
+		<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-zh-cn">
+		<div id="login">
+		<h1><a href="https://cn.wordpress.org/" title="基于WordPress" tabindex="-1">基于WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="/blog/wp-login.php" method="post">
+	<p>
+		<label for="user_login">用户名或电子邮件地址<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">密码<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> 记住我的登录信息</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="登录" />
+		<input type="hidden" name="redirect_to" value="/blog/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/blog/wp-login.php?action=lostpassword">忘记密码？</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; 返回到郭勇的技术博客</a></p>
+		
+	</div>
+
+	
+	<link rel='stylesheet' id='jetpack_css-css'  href='/blog/wp-content/plugins/jetpack/css/jetpack.css?ver=5.9' type='text/css' media='all' />
+<script type='text/javascript' src='/blog/wp-admin/load-scripts.php?c=1&amp;load%5B%5D=jquery-ui-core,jquery-ui-widget,jquery-ui-mouse,jquery-ui-sortable&amp;ver=4.9.10'></script>
+	<div class="clear"></div>
+	</body>
+	</html>
+	
